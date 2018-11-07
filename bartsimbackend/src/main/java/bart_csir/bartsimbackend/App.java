7c242406c36c7f3b2fc50dd25075318907c412ca
Tebogo Mmakgaha <mmakgtm1@gmail.com>
2018-11-07 11:55:42 +0200

    Tebogo Project Status Completed Successfully

---
+++ bartsimbackend/src/main/java/bart_csir/bartsimbackend/App.java
@@ -0,0 +1,13 @@
+package bart_csir.bartsimbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
